7fdc96379eac1e86f0d79edf1ead7356180734e9
KSH <72669002+gapalyt@users.noreply.github.com>
2023-07-02 00:27:35 +0900

    [Bronze I] Title: 팰린드롬수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -0,0 +1,11 @@
+while True:
+    number = input()
+       
+    if (number == '0'):
+        break
+        
+    elif (number == number[::-1]):
+        print("yes")
+        
+    else:
+        print("no")
